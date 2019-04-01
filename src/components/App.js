fbf2abe8fb2091005521043d1ac3894db597536c
Michael E <mickyfin10@hotmail.com>
2019-04-01 14:21:16 +0100

    positioning

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,26 @@
 import React from "react";
-import Entrance from "./Entrance";
+import Home from "../images/castle.png";
+import "./App.css";
+import { CSSTransitionGroup } from "react-transition-group";
+class App extends React.Component {
+  state = {
+    currentBackground: Home
+  };
 
-const App = () => {
-  return <Entrance />;
-};
-
+  render() {
+    return (
+      <div
+        className="cont"
+        style={{ backgroundImage: "url(" + this.state.currentBackground + ")" }}
+      >
+        <ReactCSSTransitionGroup>
+          transitionName="flash" transitionAppear={true}
+          transitionAppearTimeout={500}
+          transitionEnter={false}
+          transitionLeave={false}><p className="start">Press Enter</p>
+        </ReactCSSTransitionGroup>
+      </div>
+    );
+  }
+}
 export default App;
--- src/components/Entrance.js
+++ src/components/Entrance.js
@@ -2,13 +2,4 @@ import React from "react";
 import Background from "../images/castle.png";
 import "./entrance.css";
 
-var ImageStyle = {
-  height: "100vh",
-  width: "100vw",
-  position: "absoloute"
-};
-
-const Entrance = () => {
-  return <img src={Background} style={ImageStyle} />;
-};
 export default Entrance;
